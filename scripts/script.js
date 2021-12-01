216d7d5f235f0251a78a8de8dea070c7446429ca
Ekaterina Batist <kat.batist@yandex.ru>
2021-12-02 00:49:08 +0500

    edit after review

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,21 @@
 const popup = document.querySelector('.popup');
 const formEdit = document.querySelector('.form-edit');
 
-const openForm = document.querySelector('.openForm');
-const closeBtn = document.querySelector('.closeForm');
-const saveBtn = document.querySelector('.saveForm');
+const openForm = document.querySelector('.profile__edit-btn');
+const closeBtn = document.querySelector('.popup__close-btn');
 
 function open() {
+    let inputName = document.querySelector('.profile__name');
+    let formInputName = document.querySelector('input[id="profile-name"]');
+    formInputName.value = inputName.textContent;
+
+    let inputJob = document.querySelector('.profile__job');
+    let formInputJob = document.querySelector('input[id="profile-job"]');
+    formInputJob.value = inputJob.textContent;
+
     popup.classList.add('popup_opened');    
 }
+
 function close() {
      popup.classList.remove('popup_opened');
 }
@@ -15,27 +23,17 @@ function close() {
 openForm.addEventListener('click', open);
 closeBtn.addEventListener('click', close);
 
-let inputName = document.querySelector('.profile__name');
-let formInputName = document.querySelector('input[id="profile-name"]');
-formInputName.value = inputName.textContent;
-
-let inputJob = document.querySelector('.profile__job');
-let formInputJob = document.querySelector('input[id="profile-job"]');
-formInputJob.value = inputJob.textContent;
-
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
     formInputName = document.querySelector('input[id="profile-name"]');
     inputName = document.querySelector('.profile__name');
     inputName.textContent = formInputName.value;
+    console.log(inputName.textContent);
     formInputJob = document.querySelector('input[id="profile-job"]');
     inputJob = document.querySelector('.profile__job');
     inputJob.textContent = formInputJob.value;
-}
-
-function save() {
-    formEdit.addEventListener('submit', formSubmitHandler); 
+    console.log(inputJob.textContent);
     popup.classList.remove('popup_opened');
 }
 
-saveBtn.addEventListener('click', save);
+formEdit.addEventListener('submit', formSubmitHandler); 
