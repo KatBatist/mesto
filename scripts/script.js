c5781224f88db9fa234a754fd6765049185d7f70
Ekaterina Batist <kat.batist@yandex.ru>
2021-12-03 01:38:13 +0500

    edit review2

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,15 @@
 const popup = document.querySelector('.popup');
 const formEdit = document.querySelector('.form-edit');
-
 const openForm = document.querySelector('.profile__edit-btn');
 const closeBtn = document.querySelector('.popup__close-btn');
+const inputName = document.querySelector('.profile__name');
+const formInputName = document.querySelector('input[id="profile-name"]');
+const inputJob = document.querySelector('.profile__job');
+const formInputJob = document.querySelector('input[id="profile-job"]');
 
 function open() {
-    let inputName = document.querySelector('.profile__name');
-    let formInputName = document.querySelector('input[id="profile-name"]');
     formInputName.value = inputName.textContent;
-
-    let inputJob = document.querySelector('.profile__job');
-    let formInputJob = document.querySelector('input[id="profile-job"]');
     formInputJob.value = inputJob.textContent;
-
     popup.classList.add('popup_opened');    
 }
 
@@ -25,15 +22,9 @@ closeBtn.addEventListener('click', close);
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
-    formInputName = document.querySelector('input[id="profile-name"]');
-    inputName = document.querySelector('.profile__name');
     inputName.textContent = formInputName.value;
-    console.log(inputName.textContent);
-    formInputJob = document.querySelector('input[id="profile-job"]');
-    inputJob = document.querySelector('.profile__job');
     inputJob.textContent = formInputJob.value;
-    console.log(inputJob.textContent);
-    popup.classList.remove('popup_opened');
+    close();
 }
 
 formEdit.addEventListener('submit', formSubmitHandler); 
