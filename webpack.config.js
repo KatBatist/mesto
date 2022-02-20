89328e9be808c13b5cfccb72513acac0a5022b21
Ekaterina Batist <kat.batist@yandex.ru>
2022-02-20 11:35:20 +0500

    edit review 9_1

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     },
     mode: 'development', // добавили режим разработчика
     devServer: {
-        contentBase: path.resolve(__dirname, './dist'),
+        static: path.resolve(__dirname, './dist'),
         compress: true,
         port: 8080,
         open: true
